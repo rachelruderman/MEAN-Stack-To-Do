9f007fea99a2f29905eb21131f64fcba81ae9efa
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-03 17:17:30 -0700

    added angular2

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var mongoose = require('mongoose')
 var config = require ('./config')
 var setupController = require('./controllers/setupController')
 var port = process.env.PORT || 3000;
+var apiController = require('./controllers/apiController')
 
 app.use('/assets', express.static(__dirname + '/public'))
 
@@ -13,5 +14,6 @@ app.set('view engine', 'ejs')
 //this is the connection to mongoose
 mongoose.connect(config.getDbConnectionsString())
 setupController(app)
-
+//now express is aware of all those endpoints
+apiController(app)
 app.listen(port)
